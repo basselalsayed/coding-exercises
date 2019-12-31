f0a2ebd67202f671b2993cba41d57837ea57454f
basselalsayed <alsayed.bassel@gmail.com>
2019-12-31 19:16:11 +0300

    tests for deck class

---
+++ 21s/spec/deck_spec.rb
@@ -0,0 +1,26 @@
+require 'deck'
+
+describe Deck do 
+  let(:player) { double :player }
+
+  describe '#initialization' do 
+    it { expect(subject.cards.length).to eq 52 }
+  end
+
+  describe '#draw' do 
+    it 'removes one card from the deck when a player draws' do 
+      expect{ draw }.to change{ subject.cards.length }.by -1
+    end
+
+    it 'removes cards until there are none left' do 
+      52.times { draw }
+      expect(subject.cards.length).to eq 0
+    end
+  end
+
+  def draw
+    subject.cards.delete_at(rand(subject.cards.length))
+  end
+end
+
+
